6ef762d11bfba827672bb2b2a2996021236ff98f
derikaquary <derikaquary@gmail.com>
2024-07-31 09:16:33 +0800

    first commit

---
+++ app/app_page/page.js
@@ -0,0 +1,12 @@
+function page() {
+  return (
+    <div
+      className="flex flex-col w-full bg-green-400 gap-5 items-center justify-center mx-auto
+    ">
+      <div className="bg-red-400 h-[300px] w-[700px]"></div>
+      <div className="bg-blue-400 h-[300px] w-[700px]"></div>
+    </div>
+  );
+}
+
+export default page;
