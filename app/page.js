5e9f66952f2c73a6f622930570b53766a12f7374
derikaquary <derikaquary@gmail.com>
2024-08-03 11:49:19 +0800

    Starting to style

--- app/page.js
+++ app/page.js
@@ -1,12 +1,19 @@
 import Image from "next/image";
 import Link from "next/link";
+import background from "@/public/background.jpg";
 
 export default function Home() {
   return (
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-      <Link href="/plant_analyzer" className="h-[50px] w-[70px] bg-green-400">
-        click here
-      </Link>
-    </main>
+    <div className="flex flex-col items-center text-center gap-4">
+      <p className="text-white text-5xl">Welcome to PDC</p>
+      <p className="text-white">Plant Disease Check</p>
+      <div className="bg-white/40 px-3 py-3 rounded-2xl">
+        <Link
+          href="/plant_analyzer"
+          className=" bg-white text-3xl px-2 rounded-xl">
+          click here
+        </Link>
+      </div>
+    </div>
   );
 }
