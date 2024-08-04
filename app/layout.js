7ebddc96121d5a5073605bdf033ae738873c8c03
derikaquary <derikaquary@gmail.com>
2024-08-04 11:01:26 +0800

    update the script for adsense

--- app/layout.js
+++ app/layout.js
@@ -23,7 +23,7 @@ export default function RootLayout({ children }) {
         <script
           async
           src="https://pagead2.googlesyndication.com/pagead/js/adsbygoogle.js?client=ca-pub-5933734292382802"
-          crossOrigin="anonymous"></script>
+          crossorigin="anonymous"></script>
       </Head>
       <body className={`${playfair.className} relative min-h-screen`}>
         <div className="absolute inset-0">
