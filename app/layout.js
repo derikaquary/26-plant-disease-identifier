409913ba26c6d61d6a751f752312a66276f2f937
Derik Aquary <derikaquary@gmail.com>
2024-11-22 06:14:53 +0800

    edited the style in noscript

--- app/layout.js
+++ app/layout.js
@@ -69,7 +69,7 @@ export default function RootLayout({ children }) {
             src="https://www.googletagmanager.com/ns.html?id=GTM-N289NWHH"
             height="0"
             width="0"
-            style="display:none;visibility:hidden"
+            style={{display:"none", visibility:"hidden"}}
           ></iframe>
         </noscript>
         {/* <!-- End Google Tag Manager (noscript) --> */}
