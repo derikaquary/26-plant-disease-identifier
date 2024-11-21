30ac2044c7144dab4e63a6cf59fd1ee40223f83d
Derik Aquary <derikaquary@gmail.com>
2024-11-22 07:42:30 +0800

    added meta data to About page

--- app/about/page.js
+++ app/about/page.js
@@ -1,11 +1,18 @@
 import Navigation from "@/app/_components/Navigation.js";
 import ProfileCard from "../_components/ProfileCard";
+
+export const metadata = {
+  title: "About"
+}
+  
+
+
 function page() {
   return (
     <div className="absolute bottom-[25px] left-[25px] right-[25px] top-[25px] rounded-3xl border-r-2 border-t-2 border-white/40 bg-white/20 shadow-2xl flex flex-col gap-3 justify-center items-center ">
       <ProfileCard type="derik" name="Derik Aquary" role="Creator" />
       <div className="bg-black/30 rounded-lg w-[300px] h-[190px] overflow-auto px-3 py-2">
-        <p className="text-white text-xl">
+        <p className="text-xl text-white">
           Hello, my name is Derik. I am pleased to introduce my latest app,
           designed to assist you in identifying plant diseases and providing
           useful solutions. While I strive for accuracy, please note that the AI
