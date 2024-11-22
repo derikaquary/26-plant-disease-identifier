80cae951657be91977207156f72c816b8928047f
Derik Aquary <derikaquary@gmail.com>
2024-11-22 09:08:41 +0800

    changed sitemap form

--- next-sitemap.config.js
+++ next-sitemap.config.js
@@ -1,10 +1,20 @@
 /** @type {import('next-sitemap').IConfig} */
 
+const siteUrl = process.env.NEXT_PUBLIC_BASE_URL || 'https://plant-check.vercel.app';
+
 module.exports = {
-    siteUrl: process.env.NEXT_PUBLIC_BASE_URL || 'https://plant-check.vercel.app',
-    generateRobotsTxt: true, // Generates robots.txt file
-    changefreq: 'weekly', // Optional: Set default change frequency
-    priority: 0.7,        // Optional: Set default priority
-    sitemapSize: 5000,    // Optional: Split sitemaps if you have more than 5000 URLs
-  };
-  
+  siteUrl, // Base URL of the site
+  generateRobotsTxt: true, // Generate robots.txt file
+  exclude: ['/icon.png'], // Exclude unnecessary files or paths
+  changefreq: 'weekly', // Default change frequency for pages
+  priority: 0.7, // Default priority for pages
+  sitemapSize: 5000, // Split sitemaps if exceeding 5000 URLs
+  transform: async (config, path) => {
+    // Default transformation for regular pages
+    return {
+      loc: `${siteUrl}${path}`, // Full URL of the path
+      changefreq: config.changefreq || 'weekly',
+      priority: config.priority || 0.7,
+    };
+  },
+};
