4a4826de2f564153868b1c32c1cf81dccc01dd3c
Derik Aquary <derikaquary@gmail.com>
2024-11-22 08:09:04 +0800

    generated sitemap using next-sitemap

---
+++ next-sitemap.config.js
@@ -0,0 +1,10 @@
+/** @type {import('next-sitemap').IConfig} */
+
+module.exports = {
+    siteUrl: process.env.NEXT_PUBLIC_BASE_URL || 'https://plant-check.vercel.app',
+    generateRobotsTxt: true, // Generates robots.txt file
+    changefreq: 'weekly', // Optional: Set default change frequency
+    priority: 0.7,        // Optional: Set default priority
+    sitemapSize: 5000,    // Optional: Split sitemaps if you have more than 5000 URLs
+  };
+  
